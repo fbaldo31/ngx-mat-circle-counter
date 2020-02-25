11175d96b52e85619c2ef48dd17aa4afbaf7555b
Fred <fbaldo31@users.noreply.github.com>
2020-02-25 21:15:35 +0100

    Update depencies (#2)
    Bump to v1.0.0

--- projects/ngx-mat-circle-counter/src/lib/ngx-mat-circle-counter.module.ts
+++ projects/ngx-mat-circle-counter/src/lib/ngx-mat-circle-counter.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { NgxMatCircleCounterComponent } from './ngx-mat-circle-counter.component';
 
